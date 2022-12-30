88b6156cb8884e7818ddcc8a5dbe7c862b52123f
minyu <minyu7374@gmail.com>
2022-12-30 14:55:08 +0800

    coc-settings.json in vim

--- install.sh
+++ install.sh
@@ -9,14 +9,17 @@ function install() {
 }
 
 pro_dir="$(cd "$(dirname "$0")" && pwd)"
+vim_rc_dir="$HOME/.vim"
 nv_rc_dir="$HOME/.config/nvim"
-mkdir -p "${nv_rc_dir}"
+mkdir -p "${nv_rc_dir}" "${vim_rc_dir}"
 
 install "${pro_dir}/init.vim" "$HOME/.vimrc"
+install "${pro_dir}/coc-settings.json" "${vim_rc_dir}/coc-settings.json"
+
 install "${pro_dir}/init.vim" "${nv_rc_dir}/init.vim"
 install "${pro_dir}/coc-settings.json" "${nv_rc_dir}/coc-settings.json"
 
-echo "vim-plug install"
+echo "vim-plug install for vim & nvim"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
